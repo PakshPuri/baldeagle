b99a02aff00f043f3dc6d30a41a19947afceef4f
PakshPuri <pakshpuri3@gmail.com>
2026-06-06 19:57:34 +0530

    FINAL final final fix

--- pks.js
+++ pks.js
@@ -28,12 +28,12 @@ document.addEventListener('DOMContentLoaded', () => {
             if (text) text.style.opacity = fade;
             if (img1) img1.style.transform = `translateX(${-side}px) rotate(var(--rot, 0deg))`;
             if (img2) img2.style.transform = `translateX(${side}px) rotate(var(--rot, 0deg))`;
-            if (img3) img3.style.transform = `translateX(${side}px) rotate(var(--rot, 0deg))`;
+            if (img3) img3.style.transform = `translateX(${-side}px) rotate(var(--rot, 0deg))`;
             if (img4) img4.style.transform = `translateX(${-side}px) rotate(var(--rot, 0deg))`;
             if (img5) img5.style.transform = `translateX(${side}px) rotate(var(--rot, 0deg))`;
             if (img6) img6.style.transform = `translateX(${-side}px) rotate(var(--rot, 0deg))`;
             if (img7) img7.style.transform = `translateX(${-side}px) rotate(var(--rot, 0deg))`;
-            if (img8) img8.style.transform = `translateX(${side}px) rotate(var(--rot, 0deg))`;
+            if (img8) img8.style.transform = `translateX(${-side}px) rotate(var(--rot, 0deg))`;
         } else {
             if (text) text.style.opacity = 1;
             if (img1)  img1.style.transform  = `translate(${value * -0.4}px, ${value * -0.4}px) rotate(var(--rot, 0deg))`;
